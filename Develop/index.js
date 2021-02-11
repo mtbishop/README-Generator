1058a13617b5cf26c5edb1228fee4c86741b19d4
mtbishop <bishopmatthew76@gmail.com>
2021-02-10 22:32:00 -0600

    installed node modules and added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,61 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "readTitle",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "readDesc",
+    message: "Enter a description of your project",
+  },
+  {
+    type: "input",
+    name: "readInstall",
+    message: "Enter instructions on how to install your project",
+  },
+  {
+    type: "input",
+    name: "readUsage",
+    message: "Enter instructions on the usage of your project",
+  },
+  {
+    type: "input",
+    name: "readContrib",
+    message:
+      "Enter instructions on how to contribute to your application if applicable",
+  },
+  {
+    type: "input",
+    name: "readTest",
+    message: "Enter some test cases the user can use on your project",
+  },
+  {
+    type: "input",
+    name: "readCredit",
+    message: "Enter links and points of contact as credit on the application",
+  },
+  {
+    type: "input",
+    name: "gitHubUser",
+    message: "Enter your GitHub username",
+  },
+  {
+    type: "input",
+    name: "emailAddress",
+    message: "Enter your email address",
+  },
+  {
+    type: "",
+    name: "",
+    message: "",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
